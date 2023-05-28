d9c0ddf70b363691ae55c2ca417404a77128831a
Ehsan U <lancerf562@gmail.com>
2023-05-28 17:38:08 +0500

    add type, method, functions concepts

---
+++ deck.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// create a type
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Diamond", "Heart", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+
+}
+
+// this is method
+func (d deck) print() {
+	for _, value := range d {
+		fmt.Println(value)
+	}
+}
+
+// this is independent function
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := newDeck()
+	hand, remaining := deal(cards, 3)
+	hand.print()
+	fmt.Println("#####")
+	remaining.print()
+}
