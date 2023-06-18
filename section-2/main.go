f91025f1cb071c421e0200349442389cac3db895
Ehsan U <lancerf562@gmail.com>
2023-06-18 09:26:12 +0500

    add struct

---
+++ section-2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// define the type
+type person struct {
+	firstName string
+	lastName  string
+}
+
+func main() {
+	// declare
+	var ehsan person
+
+	// initialize
+	ehsan = person{
+		firstName: "Ehsan",
+		lastName:  "Ullah",
+	}
+
+	fmt.Println(ehsan)
+}
